ff8a5f76d5ea4e1c81487d9cbf4aeb40050eee64
Vasilis Skevakis <vskevakis@gmail.com>
2021-03-16 00:46:15 +0200

    implemented graph and dijkstra algorithm

--- app.py
+++ app.py
@@ -15,25 +15,62 @@ class Road:
     
 
 class Node:
-    def __init__(self, name, neighbours):
+    def __init__(self, name, neighbours, weight, previous, road_name):
         self.name = name
         self.neighbours = neighbours
+        self.weight = weight 
+        self.previous = previous
+        self.road_name = road_name
     
     def add_neighbour(self, node):
         neigh_list = self.neighbours
         if node not in self.neighbours:
             neigh_list.append(node)
             self.neightbours = neigh_list
+        
+    def set_weight(self, number):
+        self.weight = number
+    
+    def set_previous(self, node):
+        self.previous = node
+
+    def set_roadname(self, road_name):
+        self.road_name = road_name
 
 
 def contains(list, attribute):
+    '''
+    Takes a list and an attribute value and returns
+    if the list has a matching attribute value
+    '''
     for x in list:
         if x.name == attribute:
             return x
     return None
 
-def parse_roads(sample):
+def parse_sourcedest(sample):
+    '''
+    Parsing the samples file and returns the source
+    and the destination
+    '''
+    # Reading txt file
+    with open(sample, 'r') as file:
+        data_txt = file.read()
+    # Adding root tags to parse txt as xml
+    data = '<root>' + data_txt + '</root>'
 
+    root = ET.fromstring(data)
+
+    source = root.find('Source')
+    dest = root.find('Destination')
+
+    return source.text, dest.text
+
+def parse_roads(sample):
+    '''
+    Parsing the samples file and returns a Roads and 
+    a Nodes list
+    '''
     # Reading txt file
     with open(sample, 'r') as file:
         data_txt = file.read()
@@ -57,7 +94,7 @@ def parse_roads(sample):
         weight = attrs[3]
         roads.append(Road(road_name, node_1, node_2, weight))
         if not contains(nodes, node_1):
-            new_node_1 = Node(node_1, [])
+            new_node_1 = Node(node_1, [], None, None, None)
             new_node_1.add_neighbour(node_2)
             nodes.append(new_node_1)
         else:
@@ -66,7 +103,7 @@ def parse_roads(sample):
             node.add_neighbour(node_2)
             nodes.append(node)
         if not contains(nodes, node_2):
-            new_node_2 = Node(node_2, [])
+            new_node_2 = Node(node_2, [], None, None, None)
             new_node_2.add_neighbour(node_1)
             nodes.append(new_node_2)
         else:
@@ -79,6 +116,7 @@ def parse_roads(sample):
 
 def weight_func(roads, node_1, node_2, pred_traffic):
     lowest_weight = 999
+    lowest_road = None
     for road in roads:
         if (road.node_a == node_1 and road.node_b == node_2) or (road.node_a == node_2 and road.node_b == node_1):
             for road_p in pred_traffic:
@@ -119,7 +157,7 @@ def parse_days(sample):
         road_ind = 0
         for road in pred_road:
             prediction.append([road, pred_traffic[road_ind]])
-            road_ind = road_ind + 1
+            road_ind += 1
 
         pred_days.append(prediction)
 
@@ -135,12 +173,65 @@ def parse_days(sample):
         prediction = []
         road_ind = 0
         for road in pred_road:
-            prediction.append([road, pred_traffic[road_ind]])
-            road_ind = road_ind + 1
+            prediction.append([road,pred_traffic[road_ind]])
+            road_ind += 1
 
         actual_days.append(prediction)
 
-    return pred_days, actual_days          
+    return pred_days, actual_days        
+
+def Dijkstra(roads, nodes, source, destination, pred_traffic):
+    '''
+    Implementing a dijkstra algorith to find the shortest
+    path
+    ''' 
+    visited_nodes = 0 
+    priority_queue = []
+    for node in nodes:
+        # print(node.name)
+        if node.name == source:
+            node.set_weight(0)
+        else:
+            node.set_weight(9999)
+        node.set_previous(None)
+        priority_queue.append(node)
+
+    while priority_queue:
+        # Finding node with minimum weight
+        min_weight = 9999
+        u_node = None
+        for node in priority_queue:
+            if node.weight < min_weight:
+                u_node = node
+        visited_nodes += 1
+        priority_queue.remove(u_node)
+
+        # Terminate function
+        if u_node.name == destination:
+            path = []
+            total_weight = u_node.weight
+            while u_node.previous:
+                path = [u_node.road_name] + path
+                u_node = u_node.previous
+            return path, total_weight, visited_nodes
+
+        for neighbour in u_node.neighbours:
+            # Finding neighbour node object
+            for node in nodes:
+                if node.name == neighbour:
+                    neighbour_node = node
+                    break
+            [road_name, weight] = weight_func(roads, u_node.name, neighbour_node.name, pred_traffic)
+            alt = u_node.weight + weight
+            if alt < neighbour_node.weight:
+                neighbour_node.set_weight(alt)
+                neighbour_node.set_previous(u_node)
+                neighbour_node.set_roadname(road_name)
+        
+
+
+
+
 
 
 def main():
@@ -148,15 +239,19 @@ def main():
     This is gonna be our main
     '''
     starting_time = datetime.now()
-    samples = "samples/sampleGraph1.txt"
+    samples = "samples/sampleGraph3.txt"
 
+    [source, dest] = parse_sourcedest(samples)
     [roads, nodes] = parse_roads(samples)
     [pred_days, actual_days] = parse_days(samples)
-    
-    print(weight_func(roads, "10Node0e", "10Node7e", pred_days[0]))
+
+    # print(weight_func(roads, "10Node0e", "10Node9e", pred_days[0]))
+    for i in range(0, len(pred_days)):
+        print(Dijkstra(roads, nodes, source, dest, pred_days[i]))
+        print(Dijkstra(roads, nodes, source, dest, actual_days[i]))
 
     ending_time = datetime.now()
-    print("Total time " + str(ending_time-starting_time))
+    print("Total time: " + str(ending_time-starting_time))
 
 main()
 
